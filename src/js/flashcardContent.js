9a23e1db933c0e617456a0ca27e4046427ffa256
Tania Rascia <taniarascia@gmail.com>
2018-02-14 23:43:26 -0600

    Update signals

--- src/js/flashcardContent.js
+++ src/js/flashcardContent.js
@@ -4,524 +4,524 @@ export const flashcardContent = [
     "top": "green",
     "middle": "none",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "clear",
     "top": "green",
     "middle": "red",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "clear",
     "top": "green",
     "middle": "red",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "clear",
     "top": "green",
     "middle": "dark",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "clear",
     "top": "green",
     "middle": "dark",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "clear",
     "top": "green",
     "middle": "dark",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "clear",
     "top": "green",
     "middle": "red",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach limited",
     "top": "yellow",
     "middle": "green flashing",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "advance approach",
     "top": "yellow",
     "middle": "green",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "advance approach",
     "top": "yellow",
     "middle": "green",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "advance approach",
     "top": "yellow",
     "middle": "green",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach medium",
     "top": "yellow flashing",
     "middle": "none",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach medium",
     "top": "yellow flashing",
     "middle": "red",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach medium",
     "top": "yellow flashing",
     "middle": "dark",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach medium",
     "top": "yellow",
     "middle": "yellow",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach medium",
     "top": "yellow",
     "middle": "yellow",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach medium",
     "top": "yellow flashing",
     "middle": "dark",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach medium",
     "top": "yellow flashing",
     "middle": "red",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach medium",
     "top": "yellow",
     "middle": "yellow",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach restricting",
     "top": "yellow",
     "middle": "lunar",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach restricting",
     "top": "yellow",
     "middle": "lunar",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach restricting",
     "top": "yellow",
     "middle": "red flashing",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach restricting",
     "top": "yellow",
     "middle": "red flashing",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach restricting",
     "top": "yellow",
     "middle": "red flashing",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach",
     "top": "yellow",
     "middle": "none",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach",
     "top": "yellow",
     "middle": "red",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach",
     "top": "yellow",
     "middle": "red",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach",
     "top": "yellow",
     "middle": "dark",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach",
     "top": "yellow",
     "middle": "dark",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach",
     "top": "yellow",
     "middle": "dark",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach",
     "top": "dark",
     "middle": "yellow",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "approach",
     "top": "yellow",
     "middle": "red",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging clear",
     "top": "red",
     "middle": "green",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging clear",
     "top": "red",
     "middle": "green",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging clear",
     "top": "red",
     "middle": "red",
     "bottom": "gree",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging clear",
     "top": "red",
     "middle": "green",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging approach diverging",
     "top": "red",
     "middle": "yellow",
     "bottom": "yellow",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging approach medium",
     "top": "red",
     "middle": "yellow flashing",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging approach medium",
     "top": "red",
     "middle": "yellow flashing",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging approach medium",
     "top": "red",
     "middle": "yellow flashing",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging approach",
     "top": "red",
     "middle": "yellow",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging approach",
     "top": "red",
     "middle": "yellow",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging approach",
     "top": "red",
     "middle": "red",
     "bottom": "yellow",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "diverging approach",
     "top": "red",
     "middle": "yellow",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red flashing",
     "middle": "none",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "red flashing",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "red flashing",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red flashing",
     "middle": "red",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red flashing",
     "middle": "red",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "red",
     "bottom": "red flashing",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "dark",
     "middle": "red flashing",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "lunar",
     "middle": "none",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "lunar",
     "middle": "red",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "lunar",
     "middle": "red",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "lunar",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "red",
     "bottom": "lunar",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "lunar",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "dark",
     "middle": "lunar",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red flashing",
     "middle": "dark",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "dark",
     "middle": "red flashing",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red flashing",
     "middle": "dark",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red flashing",
     "middle": "red",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "red flashing",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "none",
     "bottom": "none",
-    "plate": "TRUE"
+    "plate": true
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "red",
     "bottom": "none",
-    "plate": "TRUE"
+    "plate": true
   },
   {
     "name": "restricting",
     "top": "red",
     "middle": "dark",
     "bottom": "none",
-    "plate": "TRUE"
+    "plate": true
   },
   {
     "name": "restricting",
     "top": "dark",
     "middle": "red",
     "bottom": "none",
-    "plate": "TRUE"
+    "plate": true
   },
   {
     "name": "stop",
     "top": "red",
     "middle": "none",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "stop",
     "top": "red",
     "middle": "red",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "stop",
     "top": "red",
     "middle": "red",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "stop",
     "top": "red",
     "middle": "dark",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "stop",
     "top": "dark",
     "middle": "red",
     "bottom": "none",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "stop",
     "top": "dark",
     "middle": "red",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "stop",
     "top": "red",
     "middle": "dark",
     "bottom": "red",
-    "plate": "FALSE"
+    "plate": false
   },
   {
     "name": "stop",
     "top": "red",
     "middle": "red",
     "bottom": "dark",
-    "plate": "FALSE"
+    "plate": false
   }
 ];
